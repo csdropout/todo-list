fd7e0c2954b3ff604beb4162b5e0f67caac939b1
qile <98936712+csdropout@users.noreply.github.com>
2026-03-18 20:16:33 +0800

    Set up default project

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import "./style.css";
 import "./modal-style.css";
-import { setUpProjectForm, initProjectFormButtons, initTaskFormButtons } from "./modals.js";
+import { setUpProjectForm, initProjectFormButtons, initTaskFormButtons, addProjectToList, displayProject} from "./modals.js";
+import Project, { projectManager } from "./project.js"; 
 
 setUpProjectForm();
 initProjectFormButtons();
 initTaskFormButtons();
+
+// set up default page
+const defaultProject = new Project("Empty Project");
+projectManager.addProject(defaultProject);
+// console.log(defaultProject);
+addProjectToList(defaultProject);
+displayProject(defaultProject);
